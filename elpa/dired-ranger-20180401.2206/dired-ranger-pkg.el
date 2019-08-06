849a52ccb82972ef2dc809657d08817b551040b0
William Ravel <wravel@gmail.com>
2019-08-06 21:38:03 +0200

    Add elpa packages

---
+++ elpa/dired-ranger-20180401.2206/dired-ranger-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-ranger" "20180401.2206" "Implementation of useful ranger features for dired" '((dash "2.7.0") (dired-hacks-utils "0.0.1")) :commit "a9dc5eb34fc96da839f5cbd70eb096758f72b3fe" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
---
+++ elpa/git-commit-20190717.29/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "6181c59b6f15145f10d5079f3c0752553f4e29f9" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
